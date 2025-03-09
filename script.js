543d6f1001b2c91fcc7ac1f1a5fcee07eeeeef96
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-03-09 15:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here aabcbb
+str= str.split("").sort().join("");
+
  for(let i = 1 ; i <str.length-1 ; i++){
      if(str[i]!==str[i-1] && str[i]!==str[i+1] ){
          return str[i];
      }
  }
+ if(str[str.length-1]!==str[str.length-2] ) return str[str.length-1];
  return null;
  
 }
